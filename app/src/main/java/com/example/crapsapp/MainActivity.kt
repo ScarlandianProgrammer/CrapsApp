feacc8ae68bee7f3f31649eb20851f1371a32f26
Elias McKee <eqbookfan@gmail.com>
2023-11-17 12:01:32 -0800

    Working on having the navbar actually show up.

--- app/src/main/java/com/example/crapsapp/MainActivity.kt
+++ app/src/main/java/com/example/crapsapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +27,16 @@ class MainActivity : AppCompatActivity() {
 
         // to make the Navigation drawer icon always appear on the action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        } else {
+            super.onSupportNavigateUp()
+        }
     }
 
     // override the onOptionsItemSelected()
