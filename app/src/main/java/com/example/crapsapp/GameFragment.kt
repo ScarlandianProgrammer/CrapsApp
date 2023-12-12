9b58c4ecfe635aeb32bee79280d49268aa3fce8f
Elias McKee <eqbookfan@gmail.com>
2023-12-12 13:05:24 -0800

    Implemented basic functionality and added some string resources.

--- app/src/main/java/com/example/crapsapp/GameFragment.kt
+++ app/src/main/java/com/example/crapsapp/GameFragment.kt
@@ -1,6 +1,7 @@
 package com.example.crapsapp
 
 import android.os.Bundle
+import android.view.Display
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,11 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class GameFragment : Fragment() {
+    private var points = 0
+    private lateinit var diceResult: TextView
+    private lateinit var pointDisplay: TextView
+    private lateinit var diceButton: Button
+    private lateinit var gameButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,20 +26,56 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        diceResult = view.findViewById(R.id.diceResult)
+        pointDisplay = view.findViewById(R.id.pointDisplay)
+        diceButton = view.findViewById(R.id.diceButton)
+        gameButton = view.findViewById(R.id.gameButton)
 
-        val diceButton: Button = view.findViewById(R.id.diceButton)
-        val diceResult: TextView = view.findViewById(R.id.diceResult)
+        gameButton.setOnClickListener {
+            startRound()
+        }
 
         diceButton.setOnClickListener {
-            val dice1 = rollDice()
-            val dice2 = rollDice()
-            val sum = dice1 + dice2
+            rollDice(diceResult, pointDisplay)
+        }
+    }
+    private fun rollDice(diceResult: TextView, pointDisplay: TextView) {
+        /*
+        * roll two dice
+        * determine if they are important
+        * display the dice total and add any points
+        */
+        val roll = (1..6).random() + (1..6).random()
+        evaluateRoll(roll)
+        updateDisplay(roll)
+    }
 
-            diceResult.text = "Dice 1: $dice1\nDice 2: $dice2\nSum: $sum"
+    private fun updateDisplay(roll: Int) {
+        pointDisplay.text = getString(R.string.points_display, points.toString())
+        if (roll == 2 || roll == 12) {
+            diceResult.text = getString(R.string.round_over_display)
+        } else {
+            diceResult.text = getString(R.string.roll_display, roll.toString())
         }
     }
 
-    private fun rollDice(): Int {
-        return (1..6).random()
+    private fun evaluateRoll(roll: Int) {
+        if (roll == 7 || roll == 11) {
+            points++
+        } else if (roll == 2 || roll == 12) {
+            endRound()
+        }
+    }
+
+    private fun startRound() {
+        diceButton.setOnClickListener {
+            rollDice(diceResult, pointDisplay)
+        }
+        points = 0
+        updateDisplay(0)
+    }
+
+    private fun endRound() {
+        diceButton.setOnClickListener {  }
     }
 }
